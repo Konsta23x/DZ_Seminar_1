d3296be71e8553b8a23614f546ea2a5113067e3a
konstantin <koycnfynby>
2022-12-20 20:09:35 +0300

    TEST_2

---
+++ TEST_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите переменную a ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите переменную b ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите переменную c ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a >= b && c < a)
+{
+    Console.Write("max = "); 
+    Console.WriteLine(a);
+}
+else if (b >= a && c < b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
+else if (c >= a && b < c)
+{
+    Console.Write("max = ");
+    Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine("Неверное значение");
+}

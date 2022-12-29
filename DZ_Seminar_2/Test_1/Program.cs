9c9526a89912de3b6c015afe2c50c1e4fad06e6d
konstantin <koycnfynby>
2022-12-29 14:05:03 +0300

    DZ_2 Test_1

---
+++ DZ_Seminar_2/Test_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на 
+// выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Выберите вариант решения ");
+int task = Convert.ToInt32(Console.ReadLine());
+int numb = new Random().Next(100, 1000);
+Console.WriteLine("Ваше число " + numb);
+
+switch (task)
+{
+    case 1:
+
+    int A = ((numb/10) % 10);
+    Console.WriteLine(A);
+    break;
+
+    case 2: 
+
+    string index = numb.ToString();
+    Console.WriteLine(index[1]);
+    break;
+}
+  

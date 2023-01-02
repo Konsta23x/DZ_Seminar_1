a968592fef590cf17a04d684688bad825a99ec6b
konstantin <koycnfynby>
2023-01-02 12:43:49 +0300

    DZ_Seminar_3

---
+++ DZ_Seminar_3/Test_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите число ");
+int i = Convert.ToInt32(Console.ReadLine());
+string index = i.ToString();
+
+    if (index[0] == index[4] && index[1] == index[3])
+        Console.WriteLine("Да ");
+    
+    else
+        Console.WriteLine("Нет");
+
---
+++ DZ_Seminar_3/Test_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними 
+// в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите значение X1 ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение Y2 ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение Z3 ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение X1 ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение Y2 ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение Z3 ");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+double Sum = Math.Round(Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2) + Math.Pow(Z2 - Z1, 2)) ,2);
+Console.WriteLine("Сумма Выраженя = " + Sum);

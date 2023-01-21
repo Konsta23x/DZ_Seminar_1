b7e12a4bb8d06b64804158fadac4d07322d32ad6
konstantin <koycnfynby>
2023-01-21 14:05:58 +0300

    DZ Seminar_4

---
+++ DZ_Seminar_4/Test_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число 
+// A в натуральную степень B. (без использования Math.Pow)
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 1;
+// for (int i = 1; i <=B; i++)
+// {
+//     sum = sum * A;
+//     Console.WriteLine(sum);
+// }
+
+
+int count = 1;
+int sum = 1;
+while (count <= B)
+{
+    sum = sum * A;
+    count++;
+}
+Console.WriteLine(sum);

cd79e9313fe89cc5772278283060c30592932cc9
konstantin <koycnfynby>
2023-02-06 22:10:07 +0300

    DZ_Seminar_8

---
+++ DZ_Seminar_8/test_2/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку 
+// с наименьшей суммой элементов.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей 
+// суммой элементов: 1 строка
+
+void FillAraay1(int[,] matrix, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+
+    }
+    Console.WriteLine();
+}
+
+void PrintArrayWithRandom(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите значение m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+FillAraay1(matrix, m, n);
+PrintArrayWithRandom(matrix);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(matrix, 0);
+for (int i = 1; i < matrix.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(matrix, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+int SumLineElements(int[,] matrix, int i)
+{
+  int sumLine = matrix[i,0];
+  for (int j = 1; j < matrix.GetLength(1); j++)
+  {
+    sumLine += matrix[i,j];
+  }
+  return sumLine;
+}
+
---
+++ DZ_Seminar_8/test_3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void FillAraay1(int[,,] matrix, int m, int n, int p)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < p; k++)
+            {
+                matrix[i, j, k] = new Random().Next(10, 100);
+
+            }
+
+        }
+
+    }
+    Console.WriteLine();
+}
+
+void PrintArrayWithRandom(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {   
+            Console.WriteLine();
+            for (int K = 0; K < matrix.GetLength(2); K++)
+            {
+                Console.Write($"{matrix[i, j, K]} ({i} {j} {K}) \t");
+            }
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+int m = 2;
+int n = 2;
+int p = 2;
+int[,,] matrix = new int[m, n, p];
+FillAraay1(matrix, m, n, p);
+PrintArrayWithRandom(matrix);

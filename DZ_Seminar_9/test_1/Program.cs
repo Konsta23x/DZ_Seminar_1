996049ff6a22f9178808b9ceedbc0868ca1d598e
konstantin <koycnfynby>
2023-02-09 20:55:59 +0300

    DZ_Seminar_9

---
+++ DZ_Seminar_9/test_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+void NumberArray(int number, int count)
+{
+    if (count > number)
+    {
+        return;
+    }
+    else
+    {
+        NumberArray(number, count + 1);
+        Console.Write(count + " ");
+    }
+}
+
+Console.WriteLine("Введите число n ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NumberArray(n, count);
---
+++ DZ_Seminar_9/test_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных
+// элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+void SumMToN(int m, int n)
+{
+    Console.Write(SumNumber(n - 1, m));
+}
+
+int SumNumber(int n, int m)
+{
+    int sum = n;
+    if (n == m)
+    {
+        return 0;
+    }
+    else
+    {
+        n++;
+        sum = n + SumNumber(n, m);
+        return sum;
+    }
+
+}
+
+Console.WriteLine("Введите число n ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число m ");
+int m = Convert.ToInt32(Console.ReadLine());
+SumMToN(m, n);
